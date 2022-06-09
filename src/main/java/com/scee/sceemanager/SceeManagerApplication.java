94e9d03698c4345c68c5d9c4fab20b95302de11c
Your Name <mehdincir@gmail.com>
2022-06-09 01:31:11 +0100

    new commit

---
+++ src/main/java/com/scee/sceemanager/SceeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.scee.sceemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SceeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SceeManagerApplication.class, args);
+    }
+
+}
